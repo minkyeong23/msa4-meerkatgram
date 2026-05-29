de1fcba4e8271ccc9e9889d76e8f38588aae6405
minkyeong23 <choi777765@gmail.com>
2026-05-29 16:51:11 +0900

    260529 [develop] 로그인 작업 완료, 리이슈 작업 완료

--- src/main/java/com/msa4meerkatgram/domain/auth/services/AuthService.java
+++ src/main/java/com/msa4meerkatgram/domain/auth/services/AuthService.java
@@ -6,13 +6,18 @@ import com.msa4meerkatgram.domain.auth.responses.AuthRes;
 import com.msa4meerkatgram.domain.user.entities.User;
 import com.msa4meerkatgram.domain.user.mapper.UserMapper;
 import com.msa4meerkatgram.domain.user.responses.UserRes;
+import com.msa4meerkatgram.global.errors.custom.InvalidTokenException;
 import com.msa4meerkatgram.global.errors.custom.NotRegisteredException;
 import com.msa4meerkatgram.global.security.cookie.CookieManager;
 import com.msa4meerkatgram.global.security.jwt.JwtConfig;
 import com.msa4meerkatgram.global.security.jwt.JwtProvider;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +27,7 @@ public class AuthService {
     private final AuthMapper authMapper;
     private final CookieManager cookieManager;
     private final JwtConfig jwtConfig;
+    private final ServletResponse servletResponse;
 
     public AuthRes login(HttpServletResponse response, LoginReq loginReq) {
         // 유저정보 획득
@@ -34,34 +40,69 @@ public class AuthService {
 
         // 비밀번호 체크
 
-        // 토큰 생성
-        String newAccessToken = jwtProvider.generateAccessToken(user);
-        String newRefreshToken = jwtProvider.generateRefreshToken(user);
-
-        // 리프레시 토큰을 DB 저장
-        authMapper.updateRefreshToken(user.getId(), newRefreshToken);
-
-        // 리프레시 토큰을 Cookie에 저장
-        cookieManager.setCookie(
-            response
-            ,jwtConfig.refreshTokenCookieName()
-            ,newRefreshToken
-            ,jwtConfig.refreshTokenCookieExpiry()
-            ,jwtConfig.reissUri()
-        );
-
-        // 리턴
-        return AuthRes.builder()
-            .accessToken(newAccessToken)
-            .user(
-                UserRes.builder()
-                    .email(user.getEmail())
-                    .nick(user.getNick())
-                    .role(user.getRole())
-                    .profile(user.getProfile())
-                    .createdAt(user.getCreatedAt())
-                    .build()
-            )
-            .build();
+        return this.generateAuthentication(response, user);
     }
+
+    public AuthRes reissue(HttpServletRequest request, HttpServletResponse response){
+        // 리프레시 토큰 획득
+        Optional<String> refreshTokenOptional = jwtProvider.extractRefreshToken(request);
+        if (refreshTokenOptional.isEmpty()) {
+            throw new InvalidTokenException("토큰이 없습니다");
+        }
+        String extractRefreshToken = refreshTokenOptional.get();
+
+        long id = Long.parseLong(jwtProvider.extractClaims(extractRefreshToken).getSubject());
+
+        // 유저 획득
+        User user = userMapper.findByPk(id);
+
+        // 유저 가입 여부 확인
+        if (user == null) {
+            throw new InvalidTokenException("유효하지 않은 회원의 토큰입니다.");
+        }
+
+        // 리프레시 토큰 비교
+        if (!user.getRefreshToken().equals(extractRefreshToken)) {
+            throw new InvalidTokenException("토큰이 일치하지 않습니다.");
+        }
+
+        return this.generateAuthentication(response, user);
+    }
+
+    /** 액세스 토큰 및 리프레시 토큰 생성후, 리프레시 토큰 DB&Cookie 에 저장, AuthRes로 반환
+    * @param response HeepServletResponse
+    * @param user 유저 Entity
+    * @return AuthRes
+    */
+     private AuthRes generateAuthentication(HttpServletResponse response, User user) {
+         // 토큰 생성
+         String newAccessToken = jwtProvider.generateAccessToken(user);
+         String newRefreshToken = jwtProvider.generateRefreshToken(user);
+
+         // 리프레시 토큰을 DB 저장
+         authMapper.updateRefreshToken(user.getId(), newRefreshToken);
+
+         // 리프레시 토큰을 Cookie에 저장
+         cookieManager.setCookie(
+             response
+             , jwtConfig.refreshTokenCookieName()
+             , newRefreshToken
+             , jwtConfig.refreshTokenCookieExpiry()
+             , jwtConfig.reissUri()
+         );
+
+         // 리턴
+         return AuthRes.builder()
+             .accessToken(newAccessToken)
+             .user(
+                 UserRes.builder()
+                     .email(user.getEmail())
+                     .nick(user.getNick())
+                     .role(user.getRole())
+                     .profile(user.getProfile())
+                     .createdAt(user.getCreatedAt())
+                     .build()
+             )
+             .build();
+     }
 }
