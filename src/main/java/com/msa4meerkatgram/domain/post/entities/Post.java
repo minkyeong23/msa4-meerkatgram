57564b1687bc941ee1b0a562de7ab959e6267e24
minkyeong23 <choi777765@gmail.com>
2026-07-06 17:26:59 +0900

    260706 JPA 전환 작업중 (기본 설정 및 엔티티 작성 완료)

--- src/main/java/com/msa4meerkatgram/domain/post/entities/Post.java
+++ src/main/java/com/msa4meerkatgram/domain/post/entities/Post.java
@@ -1,18 +1,54 @@
 package com.msa4meerkatgram.domain.post.entities;
 
-import lombok.*;
+import com.msa4meerkatgram.domain.user.entities.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@Setter
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "posts")
+@SQLDelete(sql = "UPDATE posts SET deleted_at = NOW() WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
 @Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@Setter
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "BIGINT UNSIGNED")
     private Long id;
-    private Long userId;
+
+    @Column(name = "content", nullable = false, length = 200)
     private String content;
+
+    @Column(name = "image", nullable = false, length = 100)
     private String image;
-    private String createAt;
-    private String updatedAt;
-    private String deletedAt;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at", nullable = true)
+    private LocalDateTime deletedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "user_id"
+            , insertable = true // Insert 할 때, user 객체에 어떤 값을 넣더라도, INSERT 문에 `user_id` 컬럼을 포함하지 않겠다.
+            , updatable = false // UPDATE 할 때, user 객체에 어떤 값을 넣더라도, UPDATE 문에 `user_id` 컬럼을 포함하지 않겠다.
+            , nullable = false
+            , foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT) // 물리적 FK 생성하고 싶지 않을 때
+    )
+    private User user;
 }
--- src/main/java/com/msa4meerkatgram/domain/user/entities/User.java
+++ src/main/java/com/msa4meerkatgram/domain/user/entities/User.java
@@ -1,20 +1,57 @@
 package com.msa4meerkatgram.domain.user.entities;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@Entity // 해당 클래스가 JPA 엔티티임을 선언
+@EntityListeners(AuditingEntityListener.class) // 엔티티의 이벤트 리스너 지정
+@Table(name = "users") // 테이블명 맵핑
+@SQLDelete(sql = "UPDATE users SET deleted_at = NOW() WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL") // 엔티티의 조회 시 항상 특정 조건을 추가하도록 지정
 @Getter
 @Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 자동 생성 전략 설정
+    @Column(name = "id")
     private long id;
+
+    @Column(name = "email", nullable = false, length = 50, unique = true)
     private String email;
+
+    @Column(name = "password", nullable = false, length = 255)
     private String password;
+
+    @Column(name = "nick", nullable = false, length = 20)
     private String nick;
+
+    @Column(name = "provider", nullable = false, length = 10)
     private String provider;
+
+    @Column(name = "role", nullable = false, length = 10)
     private String role;
+
+    @Column(name = "profile", nullable = false, length = 100)
     private String profile;
+
+    @Column(name = "refresh_token", nullable = true, length = 255)
     private String refreshToken;
-    private String createdAt;
-    private String updatedAt;
-    private String deletedAt;
+
+    @CreatedDate // 생성 시 자동으로 시간 입력
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate // 수정 시 자동으로 시간 업데이트
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at", nullable = true)
+    private LocalDateTime deletedAt;
 }
